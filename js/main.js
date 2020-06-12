9a6b72d3514a8598b2fae4f7192c45d7086e0e52
celestebarrios <49879625+celestebarrios@users.noreply.github.com>
2020-06-12 15:53:14 -0500

    Homework

--- js/main.js
+++ js/main.js
@@ -3,20 +3,42 @@ async function getJSON(){
     round = document.getElementsByName('round').value;
     console.log(season)
     console.log(round)
-//from class
+//from class//
     await fetch(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
         .then(data => data.json())
         .then(rawData =>{
-            //top 7 drivers
+            //top 7 drivers looping through DriverStandings
             for(let i = 0; i<7; i++){
-                let position =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position;
-                display_constructors.innerHTML = constructor_name;
-                document.body.append(display_constructors)
+                let Position =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position;
+                document.getElementById(`Position${i.toString()}`).innerHTML = Position;
             }
+            for(let i = 0; i<7; i++){
+                let last_name =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].familyName;
 
+                document.getElementById(`last_name${i.toString()}`).innerHTML = last_name;
+            }
+            for(let i = 0; i<7; i++){
+                let Nationality =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality;
+                document.getElementById(`Nationality${i.toString()}`).innerHTML = Nationality;
+            }
+            for(let i = 0; i<7; i++){
+                let Sponsor =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name;
+                document.getElementById(`Sponsor${i.toString()}`).innerHTML = Sponsor;
+            }
+            for(let i = 0; i<7; i++){
+                let Points =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
+                document.getElementById(`Points${i.toString()}`).innerHTML = Points;
+
+            }
+            
+        }) 
         .catch(error => {
             if (error) {
                 console.log('an error has occured while getting the data');
             }
         })
 }
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
