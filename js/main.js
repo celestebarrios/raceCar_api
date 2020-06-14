4f3d12c27bf7633cc038c179896103f25e4d3966
celestebarrios <49879625+celestebarrios@users.noreply.github.com>
2020-06-13 22:00:15 -0500

    almost

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 async function getJSON(){
-    year = document.getElementsByName('year').value;
-    round = document.getElementsByName('round').value;
-  
+    season = document.getElementById("season").value
+    round = document.getElementsByName("round").value
 //from class//
-    await fetch(`https://ergast.com/api/f1/${year}/${round}/driverStandings.json`)
-        .then(data => data.json())
-        .then(rawData =>{
+    await fetch(`https://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
+
+      
+    .then(data => data.json())
+    .then(rawData => {
             //top 7 drivers looping through DriverStandings
             for(let i = 0; i<7; i++){
                 let Position =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position;
                 document.getElementById(`Position${i.toString()}`).innerHTML = Position;
-                document.body.append(Position)
             }
             for(let i = 0; i<7; i++){
-                let last_name =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].familyName;
-                document.getElementById(`last_name${i.toString()}`).innerHTML = last_name;
+                let lastname =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName;
+                document.getElementById(`lastname${i.toString()}`).innerHTML = lastname;
             }
             for(let i = 0; i<7; i++){
                 let Nationality =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality;
@@ -27,16 +27,9 @@ async function getJSON(){
             for(let i = 0; i<7; i++){
                 let Points =  rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points;
                 document.getElementById(`Points${i.toString()}`).innerHTML = Points;
-
             }
             
         }) 
-        console.log(year)
-        console.log(round)
-        .catch(error => {
-            if (error) {
-                console.log('an error has occured while getting the data');
-            }
-        })
+        
 }
 
